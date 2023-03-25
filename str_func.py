e1819e12a7a7537ddfb2843519f66047dc2e21e3
AndreyVedineev <avedineev69@gmail.com>
2023-03-25 22:22:30 +0300

    add new func + docstring

--- str_func.py
+++ str_func.py
@@ -6,3 +6,8 @@ def uppercase_first_letters(letters):
     """ Первая буква заглавная"""
     return letters.capitalize()
 
+def liwer_str(letters):
+    """все маленькие"""
+    return letters.lower()
+
+
